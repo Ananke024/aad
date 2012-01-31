2b376a9e922b18c1456658156b0d4705d0ad5dc2
Mike Maas <mike.maas@gmail.com>
2012-01-30 19:02:12 -0800

    Some minor improvements to c05

--- cp210/w2012/c05/src/aad/app/c05/HelloViewActivity.java
+++ cp210/w2012/c05/src/aad/app/c05/HelloViewActivity.java
@@ -3,11 +3,15 @@ package aad.app.c05;
 import aad.app.c05.R;
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.ViewGroup.LayoutParams;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class HelloViewActivity extends Activity {
     /** Called when the activity is first created. */
@@ -15,6 +19,13 @@ public class HelloViewActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        // Display the DPI
+        DisplayMetrics outMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(outMetrics);
+        
+        ((TextView) this.findViewById(R.id.dpiTextView)).setText("Density: " + outMetrics.densityDpi + " Scaled density: " + outMetrics.scaledDensity);
+                
         
         ((CustomButton) this.findViewById(R.id.customButton1)).setOnClickListener(new OnClickListener() {
 
@@ -36,5 +47,11 @@ public class HelloViewActivity extends Activity {
             }
             
         });
+        
+        CustomView v = new CustomView(this);
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(100, 30);
+        v.setLayoutParams(params);
+        ((LinearLayout) this.findViewById(R.id.mainLinearLayout)).addView(v);
+        
     }
 }
