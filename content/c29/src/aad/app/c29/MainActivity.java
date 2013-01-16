1aa4dafe1606aca3e2fd5fd7da019f9c7f958930
Mike Maas <mike.maas@gmail.com>
2013-01-15 21:14:02 -0800

    Updates to C28 and C29

--- content/c29/src/aad/app/c29/MainActivity.java
+++ content/c29/src/aad/app/c29/MainActivity.java
@@ -2,11 +2,12 @@
 package aad.app.c29;
 
 import android.app.Activity;
-import android.content.pm.ActivityInfo;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
+import android.view.Surface;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
     
@@ -17,23 +18,20 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
-    }
-        
-
+    }        
+    
     @Override
     public void onConfigurationChanged(Configuration newConfig) {
-        Log.d(TAG, "onConfigurationChanged()");
+        Log.d(TAG, "onConfigurationChanged() orientation: " + newConfig.orientation);          
         super.onConfigurationChanged(newConfig);
         
-        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
-            //setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);            
-        }
-        else {
-            //setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
-        }
-
-        //setContentView(R.layout.activity_main);
+        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE)
+            setContentView(R.layout.activity_main);
         
+        if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT)
+            setContentView(R.layout.activity_main);
+        
+        dumpOrientationInformation();
     }
 
     @Override
@@ -58,6 +56,41 @@ public class MainActivity extends Activity {
     protected void onResume() {
         super.onResume();
         Log.d(TAG, "onResume()");
+        
+        dumpOrientationInformation();
+    }
+
+    private void dumpOrientationInformation() {
+        
+        StringBuilder sb = new StringBuilder();
+        int orientation = this.getWindowManager().getDefaultDisplay().getOrientation();
+        int rotation = this.getWindowManager().getDefaultDisplay().getRotation();
+        
+        sb.append(String.format("Orientation: %d and rotated ", orientation));
+        switch (rotation) {
+            case Surface.ROTATION_0:
+                sb.append("0");
+            break;
+            
+            case Surface.ROTATION_90:
+                sb.append("90");
+            break;
+            
+            case Surface.ROTATION_180:
+                sb.append("180");
+            break;
+            
+            case Surface.ROTATION_270:
+                sb.append("270");
+            break;
+            
+        }
+        
+        sb.append(" degrees from natural orientation");
+        
+        // Set the text of the TextView
+        TextView tv = (TextView) this.findViewById(R.id.textView2);
+        tv.setText(sb.toString());        
     }
 
     @Override
