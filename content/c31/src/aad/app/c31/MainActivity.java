f8bbef05fb52636cadc8ac0567e40d9ea0124231
Mike Maas <mike.maas@gmail.com>
2013-01-30 15:43:31 -0800

    Update to show currency usage

--- content/c31/src/aad/app/c31/MainActivity.java
+++ content/c31/src/aad/app/c31/MainActivity.java
@@ -5,16 +5,34 @@ import aad.app.c30.R;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.widget.TextView;
+
+import java.text.NumberFormat;
+import java.util.Locale;
 
 public class MainActivity extends Activity {
-    
-    private static final String TAG = "MainActivity";
-    
+        
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
+        
+        
+        long costInDollars = 10000000;
+        
+        // TODO Any exchange rate conversion...
+        
+        TextView upgradeTextView = (TextView) this.findViewById(R.id.upgradeTextView);
+        
+        Locale locale = Locale.getDefault();
+        String costFormatted = NumberFormat.getCurrencyInstance(locale).format(costInDollars);
+        
+        String upgradeString = this.getResources().getString(R.string.upgrade);
+        String costString = String.format("%s %s", upgradeString, costFormatted);
+        
+        upgradeTextView.setText(costString);      
+        
     }        
     
     @Override
